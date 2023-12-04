97022f8ec670fc5ff7fc45c1536cbf64be0ace5d
michal-dok <michaldok9@gmail.com>
2023-12-05 00:26:03 +0100

    added explanations (doesnt work well)

--- functions.py
+++ functions.py
@@ -1,9 +1,15 @@
 from sklearn.decomposition import PCA
 import numpy as np
 import matplotlib.pyplot as plt
-import keras
 from keras.models import Model
 import keract
+import lime
+from lime import lime_image
+import numpy as np
+from skimage.segmentation import slic
+from PIL import Image
+import base64
+import io
 
 def unpickle(file):
     """load dataset"""
@@ -29,16 +35,25 @@ def reduce_dim(dataset, components=2):
     reduced = model.fit_transform(dataset)
     return reduced
 
+# Function to convert image to base64 format for display in Dash
+def convert_image_to_base64(image):
+    img = Image.fromarray(image)
+    buffered = io.BytesIO()
+    img.save(buffered, format="PNG")
+    return base64.b64encode(buffered.getvalue()).decode("utf-8")
 
+def lime_explanation_generator(image, model, class_index):
+    explainer = lime_image.LimeImageExplainer()
+    # image = preprocess_image(image)
+    def predict_fn(images):
+        return model.predict(images)
+    explanation = explainer.explain_instance(image, predict_fn, labels=[class_index], top_labels=1, hide_color=0, num_samples=1000)
+    lime_image, mask = explanation.get_image_and_mask(class_index, positive_only=False, num_features=10, hide_rest=False)
+    lime_image_data = convert_image_to_base64(lime_image)
+    return lime_image_data
 
-def images2activations(data, layername):
-    input_shape = (32, 32, 3) 
-    img = rgb_images[0]
-    img = img.reshape(input_shape)
-    img_array = np.expand_dims(img, axis=0)  # Add an extra dimension for batch
+def preprocess_images(images, nsamples=1):
+    input_shape = (nsamples, 32, 32, 3) 
+    img_array = images.reshape(input_shape)
     img_array = img_array / 255.0
-
-    layer_name = "activation_19"
-
-    activations = keract.get_activations(vgg_model, img_array, layer_names=layer_name)
-    print(activations[layer_name])
+    return img_array
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import dash
 from dash import dcc, html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.express as px
 import io
 import base64
@@ -8,7 +8,6 @@ import PIL.Image
 import cv2
 from functions import *
 from sklearn.manifold import TSNE
-import keras
 import models.cifar10vgg as VGG  
 import pandas as pd
 import numpy as np
@@ -16,6 +15,10 @@ from keras.applications.vgg16 import preprocess_input
 from keras.models import load_model
 from keras.preprocessing import image
 import keract
+from skimage.segmentation import mark_boundaries
+#from tensorflow.keras.applications import inception_v3 as inc_net
+from lime import lime_image
+
 
 
 app = dash.Dash(__name__)
@@ -57,16 +60,12 @@ app.layout = html.Div([
     html.P("Number of images displayed:", style={'color': '#000000', 'font': '15px Arial', 'text-align': 'left', 'margin-left': '20px'}),
     dcc.Slider(id='input', min=0, max=len(data1[b'data']),step=500, value=500, marks={i: str(i) for i in range(0, len(data1[b'data']) + 1, 500)}),
     html.P("Image:", style={'color': '#000000', 'font': '15px Arial', 'text-align': 'left', 'margin-left': '20px'}),
-    html.Div(id='image-container', style={'text-align': 'center', 'margin-top': '20px', 'background-color': '#ADD8E6', 'padding': '20px', 'border-radius': '10px', 'margin-top': '20px'})
+    html.Div(id='image-container', style={'text-align': 'center', 'margin-top': '20px', 'background-color': '#ADD8E6', 'padding': '20px', 'border-radius': '10px', 'margin-top': '20px'}),
+    html.Button('Generate Explanation', id='generate-explanation-button'),  # Button to generate the explanation
+    
+    html.Div(id='explanation-container', style={'text-align': 'center', 'margin-top': '20px', 'background-color': '#ADD8E6', 'padding': '20px', 'border-radius': '10px', 'margin-top': '20px'})
 ])
 
-
-def preprocess_images(images, nsamples=1):
-    input_shape = (nsamples, 32, 32, 3) 
-    img_array = images.reshape(input_shape)
-    img_array = img_array / 255.0
-
-    return img_array
 
 @app.callback(
     Output('scatter-plot', 'figure'),
@@ -92,11 +91,13 @@ def update_scatter_plot(input_value):
     data = {
     'PCA_1': x,
     'PCA_2': y,
-    'Categories': labels_word
-    #'custom_variable': custom_variable[:cut]
+    'Categories': labels_word,
+    'custom_variable': custom_variable#[:cut]
     }
 
-    fig = px.scatter(data, x='PCA_1', y='PCA_2', color='Categories')# , custom_data=['custom_variable'])
+    #lime_explanation = lime_explanation_generator(image_data, model, class_index)
+
+    fig = px.scatter(data, x='PCA_1', y='PCA_2', color='Categories', custom_data=['custom_variable'])
     fig.update_layout(title_text='PCA of cifar-10 dataset', title_x=0.5)
 
     return fig
@@ -116,16 +117,61 @@ def numpy_array_to_base64(arr):
 def display_image_on_click(clickData):
     if clickData is None:
         return html.Div()  # Empty container if no point is clicked
-    else:
-        point_index = clickData['points'][0]['customdata'][0]
-        point_index = int(point_index)
-        clicked_row = images[point_index]
-        clicked_image = row2array(clicked_row)
-        w, h, d = W, H, 3
-        resized_image = cv2.resize(clicked_image, dsize=(w*10, h*10), interpolation=cv2.INTER_CUBIC)
-        image_base64 = numpy_array_to_base64(resized_image)
-        return html.Img(src=f"data:image/png;base64, {image_base64}", style={'max-width': '100%', 'height': 'auto'})
+
+    point_index = clickData['points'][0]['customdata'][0]
+    point_index = int(point_index)
+    clicked_row = images[point_index]
+    clicked_image = row2array(clicked_row)
+    w, h, d = W, H, 3
+    resized_image = cv2.resize(clicked_image, dsize=(w*10, h*10), interpolation=cv2.INTER_CUBIC)
+    image_base64 = numpy_array_to_base64(resized_image)
+    return html.Img(src=f"data:image/png;base64, {image_base64}", style={'max-width': '100%', 'height': 'auto'})
     
+@app.callback(
+    Output('explanation-container', 'children'),
+    [Input('generate-explanation-button', 'n_clicks')],
+    [State('scatter-plot', 'clickData')]
+)
+def generate_explanation(n_clicks, clickData):
+    if not (n_clicks is not None and n_clicks > 0 and clickData is not None):
+        return html.Div()  # Empty container if the conditions are not met
+
+    point_index = clickData['points'][0]['customdata'][0]
+    point_index = int(point_index)
+    clicked_row = images[point_index]
+    clicked_image = row2array(clicked_row)
+    w, h, d = W, H, 3
+    resized_image = cv2.resize(clicked_image, dsize=(w*10, h*10), interpolation=cv2.INTER_CUBIC)
+    #image_base64 = numpy_array_to_base64(resized_image)
+
+    # Generate explanations
+    explainer = lime_image.LimeImageExplainer()
+    img4explanation = images[point_index].reshape((32, 32, 3))
+    explanation = explainer.explain_instance(img4explanation.astype('double'), vgg_model.predict,  
+                                                top_labels=3, hide_color=0, num_samples=1000)
+
+    temp_1, mask_1 = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=True)
+    temp_2, mask_2 = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, num_features=10, hide_rest=False)
+
+    mask_1_resized = cv2.resize(mask_1, (clicked_image.shape[1], clicked_image.shape[0]))
+    mask_2_resized = cv2.resize(mask_2, (clicked_image.shape[1], clicked_image.shape[0]))
+
+    mask_1_3d = np.repeat(mask_1_resized[:, :, np.newaxis], 3, axis=2)
+    mask_2_3d = np.repeat(mask_2_resized[:, :, np.newaxis], 3, axis=2)
+
+    # Apply masks to the image
+    alpha = 0.5  # Adjust transparency if needed
+    combined_image = cv2.addWeighted(clicked_image.astype('float32'), 1, mask_1_3d.astype('float32'), alpha, 0)
+    combined_image = cv2.addWeighted(combined_image, 1, mask_2_3d.astype('float32'), alpha, 0)
+    
+    resized_combined_image = cv2.resize(combined_image, dsize=(w*10, h*10), interpolation=cv2.INTER_CUBIC)
+
+    cv2.imwrite('combined_image.png', resized_combined_image)
+    # Convert the combined image to base64 for display
+    _, encoded_image = cv2.imencode('.png', resized_combined_image)  # Convert to PNG format
+    combined_image_base64 =  base64.b64encode(encoded_image).decode('utf-8') 
+    # numpy_array_to_base64(combined_image) #base64.b64encode(combined_image).decode("utf-8")
+    return html.Img(src=f"data:image/png;base64, {combined_image_base64}", style={'max-width': '100%', 'height': 'auto'})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
